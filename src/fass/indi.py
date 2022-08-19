b56c6685ab67df672678869532507a2cc2407dad
T. E. Pickering <te.pickering@gmail.com>
2022-08-19 14:56:45 -0700

    add get_prop method; add INDI_Camera class

--- src/fass/indi.py
+++ src/fass/indi.py
@@ -30,6 +30,36 @@ class INDI_Device:
         else:
             self.log = log
 
+    def get_prop(self, property, key):
+        """
+        Use indi_getprop to get an INDI property
+
+        Arguments
+        ---------
+        property : str
+            INDI property of device to be queried
+
+        key : str
+            Which key of the property to query
+        """
+        cmd = ['indi_getprop', '-h', self.host, '-p', self.port]
+
+        indi_str = f"{self.devname}.{property}.{key}"
+
+        cmd.append(indi_str)
+
+        try:
+            p = subprocess.run(cmd, check=True, capture_output=True)
+        except Exception as e:
+            self.log.error(f"indi_getprop command failed: {e}")
+            return e
+
+        self.log.info(f"Get {indi_str} from {self.host}:{self.port}")
+
+        value = p.stdout.decode().split('=')[1]
+
+        return value
+
     def set_prop(self, property, key, value):
         """
         Use indi_setprop to set an INDI property
@@ -54,7 +84,7 @@ class INDI_Device:
         try:
             p = subprocess.run(cmd, check=True, capture_output=True)
         except Exception as e:
-            self.log.error(f"Command failed: {e}")
+            self.log.error(f"indi_setprop command failed: {e}")
             return e
 
         self.log.info(f"Set {indi_str} on {self.host}:{self.port}")
@@ -66,3 +96,6 @@ class INDI_Device:
 
         return p
 
+
+class INDI_Camera(INDI_Device):
+    pass
