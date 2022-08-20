462a375c7b78e71f320dbea61c4dc91c3cf5cce5
T. E. Pickering <te.pickering@gmail.com>
2022-08-19 19:35:49 -0700

    flesh out INDI_Camera class

--- src/fass/indi.py
+++ src/fass/indi.py
@@ -98,4 +98,103 @@ class INDI_Device:
 
 
 class INDI_Camera(INDI_Device):
-    pass
+    """
+    Provide higher-level interface to an INDI camera
+    """
+    def ccd_exposure(self, exptime):
+        """
+        Set exposure time for a single exposure
+
+        Arguments
+        ---------
+        exptime : float
+            Exposure time in seconds
+        """
+        self.set_prop("CCD_EXPOSURE", "CCD_EXPOSURE_VALUE", exptime)
+
+    def stream_exposure(self, exptime):
+        """
+        Set exposure time to use while streaming video
+
+        Arguments
+        ---------
+        exptime : float
+            Exposure time in seconds
+        """
+        self.set_prop("STREAMING_EXPOSURE", "STREAMING_EXPOSURE_VALUE", exptime)
+
+    def record_frames(self, nframes, savedir=None, filename=None):
+        """
+        Record a video nframes long
+
+        Arguments
+        ---------
+        nframes : int
+            Number of frames to record
+        savedir : str or None
+            (Optional) Directory to save video file to
+        filename : str or None
+            (Optional) Filename to save to
+        """
+        if savedir is not None:
+            self.set_prop("RECORD_FILE", "RECORD_FILE_DIR", savedir)
+        if filename is not None:
+            self.set_prop("RECORD_FILE", "RECORD_FILE_NAME", filename)
+
+        self.set_prop("RECORD_OPTIONS", "RECORD_FRAME_TOTAL", int(nframes))
+        self.set_prop("RECORD_STREAM", "RECORD_FRAME_ON", "On")
+
+    def record_duration(self, rectime, savedir=None, filename=None):
+        """
+        Record a video rectime seconds long
+
+        Arguments
+        ---------
+        rectime : float
+            Length of time in seconds to record for
+        savedir : str or None
+            Directory to save video file to
+        filename : str or None
+            Filename to save to
+        """
+        if savedir is not None:
+            self.set_prop("RECORD_FILE", "RECORD_FILE_DIR", savedir)
+        if filename is not None:
+            self.set_prop("RECORD_FILE", "RECORD_FILE_NAME", filename)
+
+        self.set_prop("RECORD_OPTIONS", "RECORD_DURATION", rectime)
+        self.set_prop("RECORD_STREAM", "RECORD_DURATION_ON", "On")
+
+    def ser_mode(self):
+        """
+        Configure camera to save files in SER format
+        """
+        self.set_prop("CCD_STREAM_RECORDER", "SER", "On")
+        self.set_prop("CCD_STREAM_RECORDER", "OGV", "Off")
+
+    def ogv_mode(self):
+        """
+        Configure camera to save files in OGV format
+        """
+        self.set_prop("CCD_STREAM_RECORDER", "SER", "Off")
+        self.set_prop("CCD_STREAM_RECORDER", "OGV", "On")
+
+    def set_stream_ROI(self, x, y, width, height):
+        """
+        Configure Region of Interest (ROI) to stream
+
+        Arguments
+        ---------
+        x : int
+            X position of the ROI
+        y : int
+            Y position of the ROI
+        width : int
+            Width of the ROI
+        height : int
+            Height of the ROI
+        """
+        self.set_prop("CCD_STREAM_FRAME", "X", int(x))
+        self.set_prop("CCD_STREAM_FRAME", "Y", int(y))
+        self.set_prop("CCD_STREAM_FRAME", "WIDTH", int(width))
+        self.set_prop("CCD_STREAM_FRAME", "HEIGHT", int(height))
