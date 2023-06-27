10a6fc4a91bc0d4c48429ba531bbd4e90509957d
T. E. Pickering <te.pickering@gmail.com>
2023-06-27 21:29:53 +0200

    complete porting over of salt_weather_xml.py

--- src/timdimm_tng/wx/salt_weather_xml.py
+++ src/timdimm_tng/wx/salt_weather_xml.py
@@ -1,32 +1,22 @@
-#! /usr/bin/python
 # -*- coding: utf-8 -*-
 
 from xml.dom import minidom
-from xml.etree import ElementTree as ET
-import time
-import MySQLdb as ml
-import socket
-import urllib2
-import xml.parsers.expat
-import os
+from urllib.request import urlopen
+from urllib.error import URLError
 import datetime
 
 def parse_salt_xml():
-    tcs_mode = {'0':'OFF', '1':'INITIALISE', '2':'READY', '3':'SLEW', '4':'TRACK', '5':'GUIDE', '6':'MAINTENANCE', '7':'CALIBRATION', '8':'MAJOR FAULT', '9':'SHUTDOWN'}
-
     e = {}
     # read the xml page
-    T = time.localtime()
-    localtime = '%s/%s/%s %s:%s:%s ' %(T[0],T[1],T[2],T[3],T[4],T[5])
     TCS_URL = "http://192.168.4.6/xml/salt-tcs-icd.xml"
 
     try:
-        sock = urllib2.urlopen(TCS_URL)
+        sock = urlopen(TCS_URL)
         out = open('tcs.xml', 'w')
-        out.write(sock.read())
+        out.write(sock.read().decode('utf-8'))
         out.close()
 
-    except urllib2.URLError:
+    except URLError:
         e['Valid'] = False
         return e
     #try:
@@ -41,11 +31,10 @@ def parse_salt_xml():
                 return '_'.join(tc)
             else:
                 return ''.join(rc)
-                
+
     # get all the clusters in the xml file
     maincluster = dom.getElementsByTagName('Cluster')
 
-#    print maincluster
     # read through the clusters to get get the elements and append it
     # to the dictionary
 
@@ -64,16 +53,16 @@ def parse_salt_xml():
 
             x = t[i].getElementsByTagName('Name')[0]
             y =  t[i].getElementsByTagName('Val')[0]
-            
-            name = '%s__%s' %(cluster_name, getText(x.childNodes))
-            val = '%s' %getText(y.childNodes)
-#            print name,val
+
+            name = '%s__%s' % (cluster_name, getText(x.childNodes))
+            val = '%s' % getText(y.childNodes)
+
             if t[i].nodeName == 'EW':
                 choice = t[i].getElementsByTagName('Choice')
                 d[name] = getText(choice[int(val)].childNodes)
             else:
                 d[name] = val
-                
+
     e['bms_validity'] = int(d['bms_external_conditions__validity'])
     # if the bms data validity value is zero then return an valid = False
 
@@ -82,7 +71,7 @@ def parse_salt_xml():
         return e
     else:
         pass
-    
+
     # if bms data is valid continue:
     e['DateTime'] = str(d['tcs_xml_time_info__timestamp'].replace('/','-'))
     e['Bar_Press'] = float(d['bms_external_conditions__Air_pressure']) * 10
@@ -104,33 +93,30 @@ def parse_salt_xml():
 
     e['TimeStamp_SAST'] = datetime.datetime(Year, Month, Day, Hours, Minutes, Seconds)
 
-    
     if e['Rain'] == 0:
         e['SkyCon'] = 'DRY'
     elif e['Rain'] == 1:
         e['SkyCon'] = 'RAIN'
     e['Valid'] = True
-    
-#    print e
+
     return e
 
 
 if __name__ == "__main__":
     SALT = parse_salt_xml()
-    
+
     if SALT['Valid']:
-        print ""
-        print "------------ SALT Weather Data ------------"
-        print "TimeStamp (SAST) : ", SALT['TimeStamp_SAST']
-        print "Sky Condition    : ", SALT['SkyCon']
-        print "Wind Speed (km/h): ", SALT['Wind_sp']
-        print "Wind Direction   : ", SALT['Wind_dir']
-        print "Temperature      : ", SALT['Temp']
-        print "Relative Humidity: ", SALT['Rel_Hum'], "%"
-        print "T - T(dew)       : ", float(SALT['Temp']) - float(SALT['DewTemp']) 
-        print "Barometric Press : ", SALT['Bar_Press']
-        print ""
-        print ""
-        
+        print("")
+        print("------------ SALT Weather Data ------------")
+        print("TimeStamp (SAST) : ", SALT['TimeStamp_SAST'])
+        print("Sky Condition    : ", SALT['SkyCon'])
+        print("Wind Speed (km/h): ", SALT['Wind_sp'])
+        print("Wind Direction   : ", SALT['Wind_dir'])
+        print("Temperature      : ", SALT['Temp'])
+        print("Relative Humidity: ", SALT['Rel_Hum'], "%")
+        print("T - T(dew)       : ", float(SALT['Temp']) - float(SALT['DewTemp']))
+        print("Barometric Press : ", SALT['Bar_Press'])
+        print("\n\n")
+
     else:
-        print 'Connection is down or information from BMS is invalid'
+        print('Connection is down or information from BMS is invalid')
