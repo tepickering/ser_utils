51174618d2b76752ca0f6efc2bb1bf6bac658ce5
T. E. Pickering <te.pickering@gmail.com>
2024-09-13 14:24:26 -0700

    try another stab at finding race condition

--- scripts/ekos_timdimm/postcapture.py
+++ scripts/ekos_timdimm/postcapture.py
@@ -76,8 +76,9 @@ cam.stream_exposure(exptime)
 left = max(0, int(x - 200))
 top = max(0, int(y - 200))
 cam.set_ROI(left, top, 400, 400)
+time.sleep(1)
 cam.record_duration(15, savedir="/home/timdimm", filename="seeing.ser")
-time.sleep(20)
+time.sleep(17)
 
 try:
     seeing_data = analyze_dimm_cube("/home/timdimm/seeing.ser", airmass=pointing_status['airmass'])
