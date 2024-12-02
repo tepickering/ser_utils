d9e18b3ecec338ee0b5da889deb045df1d1ccb91
T. E. Pickering <te.pickering@gmail.com>
2024-12-02 11:26:45 -0700

    set twilight limit to -6 so things get going sooner

--- scripts/ekos_timdimm/status.py
+++ scripts/ekos_timdimm/status.py
@@ -82,9 +82,9 @@ except Exception as e:
     log.error(f"Can't get current conditions: {e}")
     open_ok = False
 
-# set the safety limit to nautical twilight, -12 degrees.
+# set the safety limit to civil twilight, -6 degrees.
 # needs to be dark enough for autoguiding to be happy.
-if sun_azel.alt > -12 * u.deg:
+if sun_azel.alt > -6 * u.deg:
     open_ok = False
     if sun_azel.alt > 0 * u.deg:
         msg = f"Sun is up: {sun_azel.alt: .1f} above the horizon; "
